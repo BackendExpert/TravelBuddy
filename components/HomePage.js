179d1015ada9b7d9fa6dc502c58c536d9da9efab
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-22 06:59:05 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const HomePage = () => {
+  return (
+    <View>
+        <Text className='text-red-500'>
+            HomePage
+        </Text>
+    </View>
+  )
+}
+
+export default HomePage
