b000120c74bab276ee618219cf3c1446641690d5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-28 15:55:17 +0530

    Update Important.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- components/DashBoard/Important.js
+++ components/DashBoard/Important.js
@@ -1,14 +1,31 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const Important = ({ navigation }) => {
   return (
-    <View>
-        <Text>
-            Important Data
-        </Text>
+    <View className='mt-8 mx-4'>
+        <View style={styles.header} className='bg-gray-400/20 mt-2 py-3 rounded-full'>
+          <Text className='text-red-500 font-semibold text-xl'>
+              Important Notice
+          </Text>
+        </View>
+
+        <View className='my-4'>
+            <Text className='text-red-500 font-semibold'>
+                This is the Very Important Notice before you Travel in Sri Lanka. 
+            </Text>
+            <Text className='my-2 text-red-500 font-semibold'>
+                Read this Important Notice Carefully 
+            </Text>
+        </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  header:{
+    alignItems: 'center'
+  }
+})
 
 export default Important
