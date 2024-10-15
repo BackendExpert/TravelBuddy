82f8be2b2b1a28829e10ce16e5a4720f312500bc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-15 21:36:49 +0530

    Update

--- components/DashBoard/Sidebar.js
+++ components/DashBoard/Sidebar.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Animated, Image } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Animated, Image, Linking } from 'react-native';
 
 const Sidebar = ({ isVisible, onClose }) => {
   const translateX = useRef(new Animated.Value(-250)).current; // Sidebar initially off-screen
@@ -35,7 +35,25 @@ const Sidebar = ({ isVisible, onClose }) => {
         </TouchableOpacity>
       </View> 
 
-      <Text style={styles.sidebarText}>H</Text>
+      <View className='bg-blue-500 h-[1px] mt-2'></View>
+
+      <Text style={styles.sidebarText}>Developer Note</Text>
+
+      <Text className='mt-4'>
+        This is First (initial release) of the Project
+      </Text>
+      <Text className='mt-2'>
+        This Project will be update in Future Releases
+      </Text>
+      <Text>
+        Developer: JehanKandy
+      </Text>
+
+      <View className='mt-4'>
+          <TouchableOpacity onPress={() => Linking.openURL('https://www.linkedin.com/in/jehanweerasuriya').catch(err => console.error('An error occurred', err))}>
+            <Text>LinkedIn</Text>
+          </TouchableOpacity>
+      </View>
     </Animated.View>
   );
 };
@@ -58,7 +76,8 @@ const styles = StyleSheet.create({
   },
   sidebarText: {
     fontSize: 18,
-    color: '#333',
+    color: 'red',
+    paddingTop: 10,
   },
 
   maincontainer: {
